38c4fcbef14bbaf2b5dc78380ede0864f68df1b1
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-01-05 19:23:19 +0000

    learnt how to use data type

---
+++ main.py
@@ -0,0 +1,15 @@
+acres = 435
+message = "next_level"
+equation = 132.76
+shopping = ["apple", "banana"]
+personal_details = {
+    "name":"Yuvan",
+    "age": 10,
+    "pi_value": 3.33312,
+    "stuff": ["nintendo", "Tablet", "Cupboard"],
+}
+print(type(acres))
+print(type(message))
+print(type(equation))
+print(type(shopping))
+print(type(personal_details))
