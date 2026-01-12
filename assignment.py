875972bcf34807492b58d3fa2c33000f99978f7a
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-01-12 18:52:48 +0000

    found the type in 7 different types

---
+++ assignment.py
@@ -0,0 +1,24 @@
+acres = 13j
+story = 20.5
+people = 44
+grocer = ["ball", "bat", "stumps"]
+topplingtower = ("girl", "boy")
+variablestorer = {"name": "Sally", "gender": "female"}
+x = True
+print(type(acres))
+print(type(story))
+print(type(people))
+print(type(grocer))
+print(type(topplingtower))
+print(type(variablestorer))
+print(type(x))
+
+"""
+<class 'complex'>
+<class 'float'>
+<class 'int'>
+<class 'list'>
+<class 'tuple'>
+<class 'dict'>
+<class 'bool'>
+"""
